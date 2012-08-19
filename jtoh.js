96c82857f449090bb46d4e508db0fb3b991b0844
Dmitry Schadey <metrofun@04-0c-ce-da-56-ec.connectify>
2012-08-19 17:12:59 +0300

    Buggy compilation

--- jtoh.js
+++ jtoh.js
@@ -3,41 +3,85 @@ jtoh = (function(){
 
     var emptyTagNames = ['area', 'base', 'br', 'col', 'hr', 'img', 'input', 'link', 'meta', 'param'];
 
-    function build(json){
-        var tagName = json.tagName,
-            isEmptyTagName = emptyTagNames.indexOf(tagName) !== -1,
+    function precompile(json){
+        var tagNameRaw = json.tagName,
+            // FIXME What if a function?
+            isEmptyTagName = emptyTagNames.indexOf(tagNameRaw) !== -1,
             attributes = json.attributes,
-            htmlTokens, attrName, attrsTokens = [];
+            htmlTokens, _htmlTokens, attrName, attrValRaw, attrsTokens = [];
 
         if (Array.isArray(json)) {
             htmlTokens = json.map(function(json){
-                return build(json);
+                return precompile(json);
             });
-        } else {
+        } else if (typeof json === 'object') {
             if (json.className) {
                 attributes = attributes || {};
                 attributes['class'] = json.className;
             }
 
             if (isEmptyTagName) {
-                htmlTokens = ['<', tagName, '/>'];
+                htmlTokens = ['<', tagNameRaw, '/>'];
+                if (typeof tagNameRaw === 'function') {
+                    _htmlTokens = [function() {
+                        var tagName = htmlTokens.tagNameRaw.apply(this, arguments);
+                        htmlTokens[1] = tagName;
+                        return tagName? htmlTokens:[];
+                    }]
+                }
             } else {
-                htmlTokens = ['<', tagName, '>', '</', tagName, '>'];
-                htmlTokens.splice(3, 0, build(json.innerHTML));
+                htmlTokens = ['<', tagNameRaw, '>', '</', tagNameRaw, '>'];
+                htmlTokens.splice.apply(htmlTokens, [3, 0].concat(precompile(json.innerHTML)));
+                if (typeof tagNameRaw === 'function') {
+                    _htmlTokens = [function() {
+                        var tagName = tagNameRaw.apply(this, arguments);
+                        htmlTokens[1] = tagName;
+                        htmlTokens[htmlTokens.length - 2] = tagName;
+                        return tagName? htmlTokens:[];
+                    }]
+                }
+
             }
 
             for (attrName in attributes) {
-                // TODO escape double quotes
-                attrsTokens = attrsTokens.concat([' ', attrName, '="', attributes[attrName], '"']);
+                var attrValRaw = attributes[attrName];
+                if (typeof attrValRaw === 'function') {
+                    attrsTokens = attrsTokens.concat(function(attrValRaw) {
+                        var attrVal = attrValRaw.apply(this, [].slice.call(arguments, 1));
+                        // TODO escape double quotes
+                        return attrVal? [' ', attrName, '="', attrVal, '"']:[];
+                    }.bind(this, attrValRaw));
+                } else {
+                    // TODO escape double quotes
+                    attrsTokens = attrsTokens.concat([' ', attrName, '="', attrValRaw, '"']);
+                }
             }
             htmlTokens.splice.apply(htmlTokens, [2, 0].concat(attrsTokens));
+        } else {
+            htmlTokens = [json];
         }
 
-        return htmlTokens.join('');
-
+        return _htmlTokens || htmlTokens;
     };
+    function compile(json) {
+        var precompiled = precompile(json);
+        return function build(precompiled, data){
+            var compiled = precompiled.map(function(strRaw){
+                return (typeof strRaw === 'function')?build(strRaw.apply(this, data), data):strRaw;
+            });
+            return compiled.join('');
+        }.bind(this, precompiled);
+    }
 
     return {
-        build: build
+        compile: compile
     };
 })();
+console.log(jtoh.compile({
+    tagName: function(){return 'img'},
+    attributes: {
+        zz: 123,
+        yy: function() {return 0;}
+    },
+    innerHTML: 'uuuuu'
+})());
